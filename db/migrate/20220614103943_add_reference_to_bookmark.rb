c3102bf97b03ce662f00a212d08c1207a962c825
Raghave Gupta <raghave18@gmail.com>
2022-06-14 21:19:24 +1000

    task 1 done

--- db/migrate/20220614103943_add_reference_to_bookmark.rb
+++
@@ -1,6 +0,0 @@
-class AddReferenceToBookmark < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :bookmark, :movie, foreign_key: true
-    add_reference :bookmark, :list, foreign_key: true
-  end
-end
